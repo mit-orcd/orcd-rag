b16da2dd562ca1aef76680e45b46d9e2ba26ea8b
secorey <secorey@mit.edu>
2025-02-14 16:14:13 -0500

    updating container

--- container/run_container.sh
+++ container/run_container.sh
@@ -7,6 +7,7 @@ REPO_PATH=/orcd/datasets/001/orcd-rag
 # Set HF_HOME if it is not set:
 if [ -z "${HF_HOME}" ]; then
     export HF_HOME=$HOME/.cache/huggingface
+fi
 
 apptainer exec --nv \
                --env HF_TOKEN=$HF_TOKEN \
@@ -15,5 +16,3 @@ apptainer exec --nv \
                --bind $REPO_PATH:/tmp/orcd-rag \
                $REPO_PATH/container/rag_container.sif \
                python /tmp/orcd-rag/rag.py
-
-# To do: Add option for flags
