c062ec9c14753c6ff1b058b0825ff819a7b3a677
secorey <secorey@mit.edu>
2025-03-11 15:29:08 -0400

    changing path to .sif

--- container/run_container.sh
+++ container/run_container.sh
@@ -5,6 +5,14 @@ module load apptainer
 # Get the path to the RAG directory:
 REPO_PATH=$(dirname "$(dirname "$(realpath "$0")")")
 
+# Set path to global .sif image if it exists:
+if [ -f "/orcd/software/community/001/container_images/orcd-rag/20250311/rag_container.sif" ]; then
+    SIF_PATH=/orcd/software/community/001/container_images/orcd-rag/20250311/rag_container.sif
+else
+    SIF_PATH=$REPO_PATH/container/rag_container.sif
+fi
+echo "Using image located at ${SIF_PATH}"
+
 # Set HF_HOME if it is not set:
 if [ -z "${HF_HOME}" ]; then
     export HF_HOME=$HOME/.cache/huggingface
@@ -18,5 +26,5 @@ apptainer exec --nv \
                --env HF_HOME=/tmp/.cache/huggingface \
                --bind $HF_HOME:/tmp/.cache/huggingface \
                --bind $REPO_PATH:/tmp/orcd-rag \
-               $REPO_PATH/container/rag_container.sif \
+               $SIF_PATH \
                python /tmp/orcd-rag/rag.py
